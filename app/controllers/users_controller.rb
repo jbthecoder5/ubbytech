79cbbbc41e8faee34f402a6f94662a67454a8588
Joseph P Bourne <kiratsuchi@live.com>
2017-02-24 14:58:18 -0800

    sendgrid email worked perfectly. almost done with UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,21 +27,13 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
         # deliver the signup email
-        UserNotifier.send_signup_email(@user).deliver
+        UserNotifierMailer.send_signup_email(@user).deliver
         redirect_to(@user, :notice => 'User created')
     else
       render :action => 'new'
     end
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    
   end
 
   # PATCH/PUT /users/1
