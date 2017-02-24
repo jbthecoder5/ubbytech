2542f92bbdd85cf3ad740e7af21dbd59c14ae23d
Joseph P Bourne <kiratsuchi@live.com>
2017-02-24 12:07:46 -0800

    footer almost done, generated a user scaffold for sendgrid gem

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'josephpbourne',
+  :password => 'H061290m103!',
+  :domain => 'www.ubbytech.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
