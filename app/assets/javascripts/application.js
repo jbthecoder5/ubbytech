2a700efba1290924ddcb472c83f92125ddab1d6b
Joseph P Bourne <kiratsuchi@live.com>
2017-10-26 22:10:41 -0700

    using production file of particles.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require particles.min
+//= require particles
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
