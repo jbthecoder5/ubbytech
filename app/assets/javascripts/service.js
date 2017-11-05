ef73e42560d9f2fd93a96617861154a1176258e5
Joseph P Bourne <kiratsuchi@live.com>
2017-11-05 10:10:54 -0800

    another precompile for new js file

--- app/assets/javascripts/service.js
+++ app/assets/javascripts/service.js
@@ -1,36 +1,41 @@
-$(function() {
+document.addEventListener("turbolinks:load", function() {
 
-    var $sidebar   = $("#side-nav"), 
-        $nav_icon  = $("#nav-icon"),
-        $window    = $(window),
-        offset     = $sidebar.offset(),
-        offset2    = $nav_icon.offset(),
-        topPadding = 15;
+  console.log("Loading service.js");
 
-    $window.scroll(function() {
-        if ($window.scrollTop() > offset.top && $window.scrollTop() > offset2.top ) {
-            $sidebar.stop().animate({
-                marginTop: $window.scrollTop()
-            }, 50, function(){
+    $(function() {
 
-            });
-            $nav_icon.stop().animate({
-                marginTop: $window.scrollTop()
-            }, 50, function(){
+        var $sidebar   = $("#side-nav"), 
+            $nav_icon  = $("#nav-icon"),
+            $window    = $(window),
+            offset     = $sidebar.offset(),
+            offset2    = $nav_icon.offset(),
+            topPadding = 15;
 
-            });
-        } else {
-            $sidebar.stop().animate({
-                marginTop: 0
-            }, 50, function(){
+        $window.scroll(function() {
+            if ($window.scrollTop() > offset.top && $window.scrollTop() > offset2.top ) {
+                $sidebar.stop().animate({
+                    marginTop: $window.scrollTop()
+                }, 50, function(){
 
-            });
-            $nav_icon.stop().animate({
-                marginTop: 0
-            }, 50, function(){
+                });
+                $nav_icon.stop().animate({
+                    marginTop: $window.scrollTop()
+                }, 50, function(){
 
-            });
-        }
+                });
+            } else {
+                $sidebar.stop().animate({
+                    marginTop: 0
+                }, 50, function(){
+
+                });
+                $nav_icon.stop().animate({
+                    marginTop: 0
+                }, 50, function(){
+
+                });
+            }
+        });
+        
     });
-    
 });
