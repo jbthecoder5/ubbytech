4f1ac0efb0b698ab83ff913de1328b2ac8ab3bbb
Joseph P Bourne <kiratsuchi@live.com>
2017-11-05 09:53:42 -0800

    small changes to contact and service.js file and slide-nav

---
+++ app/assets/javascripts/service.js
@@ -0,0 +1,36 @@
+$(function() {
+
+    var $sidebar   = $("#side-nav"), 
+        $nav_icon  = $("#nav-icon"),
+        $window    = $(window),
+        offset     = $sidebar.offset(),
+        offset2    = $nav_icon.offset(),
+        topPadding = 15;
+
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.top && $window.scrollTop() > offset2.top ) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop()
+            }, 50, function(){
+
+            });
+            $nav_icon.stop().animate({
+                marginTop: $window.scrollTop()
+            }, 50, function(){
+
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            }, 50, function(){
+
+            });
+            $nav_icon.stop().animate({
+                marginTop: 0
+            }, 50, function(){
+
+            });
+        }
+    });
+    
+});
