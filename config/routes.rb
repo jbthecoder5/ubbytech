e90038d54a50699455dc68d7c951a0cb725ff664
Joseph P Bourne <kiratsuchi@live.com>
2017-11-11 17:08:47 -0800

    made change to routes file, some small changes to contact page and scss file

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/services', to: 'pages#services', as: '/services'
   get '/about', to: 'pages#about', as: '/about'
   get '/portfolio', to: 'pages#portfolio', as: '/portfolio'
-  get '/contact', to: 'pages#contact', as: '/contact'
+  get '/contact-us', to: 'pages#contact-us', as: '/contact-us'
   get '/blog', to: 'pages#blog', as: '/blog'
   
 end
