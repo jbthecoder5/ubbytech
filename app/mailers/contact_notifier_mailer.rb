259e22b47ba9ae02ad151a0db2fa4a4ec4718ebf
Joseph P Bourne <kiratsuchi@live.com>
2018-01-15 15:27:18 -0800

    made some changes to the contact mailer notifications

--- app/mailers/contact_notifier_mailer.rb
+++ app/mailers/contact_notifier_mailer.rb
@@ -1,9 +1,12 @@
 class ContactNotifierMailer < ApplicationMailer
-  default :from => 'ubbytech@gmail.com'
+  default :from => 'ubbytech@gmail.com',
+          :bcc => 'ubbytech@gmail.com',
+          :reply => 'ubbytech@gmail.com'
+
 
   def send_signup_email(contact)
     @contact = contact
     mail( :to => @contact.email,
-    :subject => 'Thanks for sending your message. Will respond within 12-24 hours')
+    :subject => 'UbbyTech :: Your ticket hass been received')
   end
 end
