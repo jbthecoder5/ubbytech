41fb99d20ace6ed6bf5d209221e544202f1fb641
Joseph Bourne <josephpbourne@gmail.com>
2019-12-11 11:56:44 -0800

    Update environment.rb
    removed my sign-in credentials from sendgrid.com

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,11 @@ Rails.application.initialize!
 
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'josephpbourne',
-  :password => 'H061290m103!',
-  :domain => 'ubbytech.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
+  :user_name => '',
+  :password => '',
+  :domain => '',
+  :address => '',
+  :port => 555,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
